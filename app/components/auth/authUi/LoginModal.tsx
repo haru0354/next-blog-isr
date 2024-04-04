98c4a8d292c4f58d7234d8f04fd453686984da71
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-04 20:48:48 +0900

    authでのログイン・ブログとアプリが1つできる

---
+++ app/components/auth/authUi/LoginModal.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+
+import toast from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import useSignupModal from "../hooks/useSignupModal";
+import useLoginModal from "../hooks/useLoginModal";
+import Modal from "./Modal";
+import AuthInput from "./AuthInput";
+import Button from "../../ui/Button";
+
+const schema = z.object({
+  email: z.string().email({ message: "メールアドレスの形式ではありません。" }),
+  password: z.string().min(6, { message: "6文字以上入力する必要があります。" }),
+});
+
+const LoginModal = () => {
+  const router = useRouter();
+  const signupModal = useSignupModal();
+  const loginModal = useLoginModal();
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: { email: "", password: "" },
+    resolver: zodResolver(schema),
+  });
+
+  const onToggle = useCallback(() => {
+    loginModal.onClose();
+    signupModal.onOpen();
+  }, [loginModal, signupModal]);
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    setLoading(true);
+    try {
+      const res = await signIn("itinerary", {
+        ...data,
+        redirect: false,
+      });
+
+      if (res?.error) {
+        toast.error("エラーが発生しました。" + res.error);
+        setErrorMessage(res.error);
+        return;
+      }
+
+      toast.success("ログインしました！");
+      loginModal.onClose();
+      router.replace(`/memorybook/home`);
+    } catch (error) {
+      toast.error("エラーが発生しました。" + error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const bodyContent = (
+    <div>
+      <AuthInput
+        id="email"
+        label="メールアドレス"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <AuthInput
+        id="password"
+        label="パスワード"
+        type="password"
+        disabled={loading}
+        register={register}
+        errors={errors}
+        required
+      />
+      {errorMessage && <p className="text-red-500 pt-4 px-4">{errorMessage}</p>}
+    </div>
+  );
+
+  const footerContent = (
+    <div className="w-full px-4 my-6 ">
+      <Button
+        onClick={() =>
+          signIn("google", {
+            callbackUrl: "/memorybook/home",
+          })
+        }
+        color="gray"
+        size="auth"
+        className=" rounded mt-4"
+      >
+        Googleでログイン
+      </Button>
+
+      <div className="my-8 border-t border-dashed border-gray-600">
+        <p
+          onClick={onToggle}
+          className="text-center text-gray-600 pt-2 cursor-pointer "
+        >
+          アカウントを作成する
+        </p>
+      </div>
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={loading}
+      isOpen={loginModal.isOpen}
+      title="ログイン"
+      primaryLabel="ログイン"
+      onClose={loginModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default LoginModal;
---
+++ app/components/auth/navigation/Menu.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useCallback, useState, useEffect } from "react";
+import { signOut } from "next-auth/react";
+
+import useLoginModal from "../hooks/useLoginModal";
+import useSignupModal from "../hooks/useSignupModal";
+
+type MenuProps = {
+  currentUser: number | null;
+};
+
+const Menu: React.FC<MenuProps> = ({ currentUser }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const loginModal = useLoginModal();
+  const signupModal = useSignupModal();
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [currentUser]);
+
+  return (
+    <>
+      {currentUser ? (
+        <>
+          <div style={{ position: "relative" }}>
+            <div onClick={toggleOpen}>
+              <Image
+                src="/user_logo01.png"
+                alt="アバター"
+                width={50}
+                height={50}
+              />
+            </div>
+            {isOpen && (
+              <ul className="absolute right-0 mt-2 w-40 p-2 bg-white  overflow-hidden shadow-lg z-10 text-base ">
+                <li className="cursor-pointer text-sky-700 hover:bg-gray-200 p-2">
+                  <Link href="/memorybook/home">管理画面</Link>
+                </li>
+                <li
+                  onClick={() => {
+                    signOut();
+                    setIsOpen(false);
+                  }}
+                  className="cursor-pointer text-sky-700 hover:bg-gray-200 p-2"
+                >
+                  ログアウト
+                </li>
+              </ul>
+            )}
+          </div>
+        </>
+      ) : (
+        <ul className="flex">
+          <li className="ml-4">
+            <p
+              className="cursor-pointer mb-0"
+              onClick={() => {
+                loginModal.onOpen();
+                setIsOpen(true);
+              }}
+            >
+              ログイン
+            </p>
+          </li>
+          <li className="ml-4">
+            <p
+              className="cursor-pointer mb-0"
+              onClick={() => {
+                signupModal.onOpen();
+                setIsOpen(true);
+              }}
+            >
+              登録
+            </p>
+          </li>
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Menu;
