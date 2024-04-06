18a52747831c913c37cff46e2a8069a4325d1ab1
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-06 14:14:30 +0900

    3種類のボタンの作成

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  formAction?: (data: FormData) => Promise<{ message: string } | undefined>;
+  onClick?: () => void;
+  className?: string;
+  color: "blue" | "gray" | "red" | "white";
+  size: "normal" | "small" | "auth";
+};
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  formAction,
+  onClick,
+  className,
+  color,
+  size,
+}) => {
+  const { pending } = useFormStatus();
+
+  const colors = {
+    blue: "border-sky-900 bg-sky-700",
+    gray: "border-gray-900 bg-gray-700",
+    red: "border-red-900 bg-red-700",
+    white: "border-gray-900 bg-gray-500",
+  };
+
+  const sizes = {
+    normal: "min-w-[170px] px-10 py-2 block mx-auto",
+    small: "min-w-[100px] px-6 py-1 mx-2",
+    auth: "min-w-[250px] px-10 py-2 block mx-auto"
+  };
+
+  return (
+    <button
+      className={` shadow font-bold border text-white hover:text-black hover:bg-white 
+      ${className} 
+      ${colors[color]} 
+      ${sizes[size]} 
+      ${pending ? "opacity-30" : ""}`}
+      type="submit"
+      formAction={formAction}
+      disabled={pending}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
