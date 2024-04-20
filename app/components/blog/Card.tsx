39b2b129816b835cea8147e12300b283c508f612
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-20 23:21:05 +0900

    関連記事用のカードの作成

---
+++ app/components/blog/Card.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+
+type Post = {
+  title: string;
+  description: string;
+  draft: boolean;
+  postImage?: {
+    url: string;
+    altText: string;
+  } | null;
+};
+
+type CardProps = {
+  post: Post;
+};
+
+const Card: React.FC<CardProps> = ({ post }) => {
+  return (
+    <>
+      {post.draft && (
+        <div className="flex flex-wrap md:flex-nowrap  border w-full border-gray-400 my-8 hover:bg-gray-100">
+          {post.postImage ? (
+            <div className="w-full md:w-auto pt-3 md:pt-0 min-w-[260px]">
+              <Image
+                src={post.postImage.url}
+                alt={post.postImage.altText}
+                width={260}
+                height={171}
+                style={{
+                  width: "260px",
+                  height: "auto",
+                }}
+                className="block mx-auto"
+              />
+            </div>
+          ) : (
+            <div className="w-full md:w-auto pt-3 md:pt-0 min-w-[260px]">
+              <Image
+                src="/no_image.jpg"
+                alt="画像の準備中"
+                width={328}
+                height={213}
+                style={{
+                  width: "261px",
+                  height: "auto",
+                }}
+                className="block mx-auto"
+              />
+            </div>
+          )}
+          <div className="w-full md:flex-auto px-4 py-3 card">
+            {post.title && post.title.length > 31 ? (
+              <h3 className="text-lg font-bold text-gray-700 mb-4">
+                {post.title.slice(0, 31)}...
+              </h3>
+            ) : (
+              <h3 className="text-lg font-bold text-gray-700 mb-4 border-none ">
+                {post.title}
+              </h3>
+            )}
+            <p className="text-gray-600">
+              {post.description.length > 72
+                ? `${post.description.slice(0, 72)}...`
+                : post.description}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Card;
