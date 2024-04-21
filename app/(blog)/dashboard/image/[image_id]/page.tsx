0c89546bb72a9a10d3f8c3160c20a08cb9967b26
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-21 23:10:30 +0900

    ダッシュボードの画像ライブラリの追加・削除・編集

---
+++ app/(blog)/dashboard/image/[image_id]/page.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+import FormPostImage from "@/app/components/blog/dashboard/FormPostImage";
+import Button from "@/app/components/ui/Button";
+import DeleteModal from "@/app/components/ui/DeleteModal";
+
+import {
+  deletePostImage,
+  updatePostImage,
+} from "@/app/action/action-postImage";
+import { getPostImage } from "@/app/components/lib/BlogServiceUnique";
+
+const page = async ({ params }: { params: { image_id: string } }) => {
+  const id = Number(params.image_id);
+  const updatePostImageWidthId = updatePostImage.bind(null, id);
+  const postImage = await getPostImage(params.image_id)
+
+  return (
+    <>
+      <h2 className="bg-gray-700 text-xl bold text-white rounded mb-12 p-5 font-bold">
+        画像の編集
+      </h2>
+      <FormPostImage
+        formAction={updatePostImageWidthId}
+        postImage={postImage}
+        buttonName="編集内容を保存"
+      />
+      <Link href="/dashboard/image/">
+        <Button color="gray" size="normal" className="rounded mt-4">
+          キャンセル
+        </Button>
+      </Link>
+      <DeleteModal
+        DeleteName="画像"
+        name={postImage?.name}
+        formAction={deletePostImage}
+        id={postImage?.id}
+      />
+    </>
+  );
+};
+
+export default page;
