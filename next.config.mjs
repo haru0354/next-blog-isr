566eafda6907d06eb8f6805eef8dc37ce3bc5a48
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-07-20 22:44:13 +0900

    configにだmainsでsupabaseの画像のドメインを追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['ybfrmhradztqizzobkvg.supabase.co'],
+    },
+  };
 
 export default nextConfig;
