75c005f46bfe0afac50c6f8f40c478dcc43721c9
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-07-21 23:08:53 +0900

    サイトマップの作成

---
+++ app/sitemaps/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export const metadata: Metadata = {
+  title: "プライバシーポリシー・免責事項 | 編集箇所サイト名",
+  robots: {
+    index: false,
+  },
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      <main className="bg-sky-50">
+        <div className="main-contents-area rounded">
+          <div className="w-full px-1 md:px-8">{children}</div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
