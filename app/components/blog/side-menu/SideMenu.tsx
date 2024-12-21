1450bca154044b31c7783f1c7fb2bdffa5771f35
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-12-21 12:56:00 +0900

    サイドメニューの画像リンクを使いまわせるように変更して上と下に設置

--- app/components/blog/side-menu/SideMenu.tsx
+++ app/components/blog/side-menu/SideMenu.tsx
@@ -1,13 +1,24 @@
-import SideTopImage from "./parts/SideTopImage";
+import SideImage from "./parts/SideImage";
 import SideCategoryMenu from "./parts/SideCategoryMenu";
 import SideNewArticles from "./parts/SideNewArticles";
 
 const SideMenu = () => {
   return (
     <div className="flex flex-col justify-center items-center bg-white rounded py-2">
-      <SideTopImage />
+      <SideImage
+        src="/no_image.jpg"
+        alt="画像がありません"
+        href="/"
+        textTitle=""
+      />
       <SideCategoryMenu />
       <SideNewArticles />
+      <SideImage
+        src="/no_image.jpg"
+        alt="画像がありません"
+        href="/"
+        textTitle="テキスト有サイド画像リンク"
+      />
     </div>
   );
 };
--- app/components/blog/side-menu/parts/SideImage.tsx
+++ app/components/blog/side-menu/parts/SideImage.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const SideTopImage = () => {
+type SideImageProps = {
+  src: string;
+  alt: string;
+  href: string;
+  textTitle?: string;
+};
+
+const SideImage: React.FC<SideImageProps> = ({
+  src,
+  alt,
+  href,
+  textTitle,
+}) => {
   return (
     <>
       <div className="hover:bg-gray-200 mb-4">
-        <Link href="/">
+        <Link href={href}>
           <Image
-            src="/no_image.jpg"
-            alt="海外旅行のしおりアプリ"
+            src={src}
+            alt={alt}
             width={240}
             height={174}
             style={{
@@ -17,12 +29,11 @@ const SideTopImage = () => {
             }}
             className="block mx-auto"
           />
-          <p className="my-2 text-center">サイドトップ画像リンク
-          </p>
+          {textTitle && <p className="my-2 text-center">{textTitle}</p>}
         </Link>
       </div>
     </>
   );
 };
 
-export default SideTopImage;
+export default SideImage;
