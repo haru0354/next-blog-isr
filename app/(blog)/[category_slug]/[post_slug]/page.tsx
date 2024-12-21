523b83339f5e08fcc9664b2d77ad202e01c73aac
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-12-21 13:36:01 +0900

    関連記事を個別記事ページにも表示

--- app/(blog)/[category_slug]/[post_slug]/page.tsx
+++ app/(blog)/[category_slug]/[post_slug]/page.tsx
@@ -1,10 +1,12 @@
-import { getPost } from "@/app/lib/service/blogServiceUnique";
+import { getCategory, getPost } from "@/app/lib/service/blogServiceUnique";
 import { getPosts } from "@/app/lib/service/blogServiceMany";
 import ArticleContentArea from "@/app/components/blog/contents-area/ArticleContentArea";
 import ArticleTop from "@/app/components/blog/contents-area/ArticleTop";
 import Breadcrumbs from "@/app/components/blog/contents-area/Breadcrumbs";
 import SideMenu from "@/app/components/blog/side-menu/SideMenu";
 import NotFound from "@/app/not-found";
+import Card from "@/app/components/blog/Card";
+import Link from "next/link";
 
 export async function generateStaticParams() {
   const posts = await getPosts("categoryAndPostImage");
@@ -13,11 +15,15 @@ export async function generateStaticParams() {
     params: {
       post_slug: post.slug,
     },
-    revalidate: 60 * 60 * 24 * 15, 
+    revalidate: 60 * 60 * 24 * 15,
   }));
 }
 
-const Page = async ({ params }: { params: { post_slug: string } }) => {
+const Page = async ({
+  params,
+}: {
+  params: { post_slug: string; category_slug: string };
+}) => {
   const post = await getPost("slug", params.post_slug, "categoryAndPostImage");
 
   if (!post || post.draft === false) {
@@ -30,6 +36,27 @@ const Page = async ({ params }: { params: { post_slug: string } }) => {
   }
 
   const formattedCreatedDate = new Date(post.createdDate).toLocaleDateString();
+
+  const category = await getCategory(
+    "slug",
+    params.category_slug,
+    "postsAndPostImage"
+  );
+
+  if (
+    !category ||
+    (!category.title && category.posts.every((post) => !post.draft))
+  ) {
+    return (
+      <div>
+        <NotFound />
+        <p>カテゴリが存在しないか削除された可能性があります。</p>
+      </div>
+    );
+  }
+  const filteredCategoryInArticle = category.posts.filter(
+    (post) => post.slug !== params.post_slug
+  );
 
   return (
     <>
@@ -44,6 +71,14 @@ const Page = async ({ params }: { params: { post_slug: string } }) => {
           記事の投稿日：{formattedCreatedDate}
         </p>
         <ArticleContentArea content={post.content} />
+        <h2 className="p-2 mt-10 text-3xl">関連記事</h2>
+        {filteredCategoryInArticle.map((article) => {
+          return (
+            <Link href={`/${category.slug}/${article.slug}`} key={post.id}>
+              <Card post={article} />
+            </Link>
+          );
+        })}
       </div>
       <div className="w-full md:w-1/4 py-4 bg-white rounded">
         <SideMenu />
--- app/components/blog/Card.tsx
+++ app/components/blog/Card.tsx
@@ -18,53 +18,53 @@ const Card: React.FC<CardProps> = ({ post }) => {
   return (
     <>
       {post.draft && (
-                <div className="flex flex-wrap md:flex-nowrap  border w-full border-gray-400 my-8 hover:bg-gray-100">
-                {post.postImage ? (
-                  <div className="w-full md:w-auto pt-3 md:pt-0 min-w-[300px]">
-                    <Image
-                      src={post.postImage.url}
-                      alt={post.postImage.altText}
-                      width={300}
-                      height={200}
-                      style={{
-                        width: "300",
-                        height: "auto",
-                      }}
-                      className="block mx-auto"
-                    />
-                  </div>
-                ) : (
-                  <div className="w-full md:w-auto pt-3 md:pt-0 min-w-[300px]">
-                    <Image
-                      src="/no_image.jpg"
-                      alt="画像の準備中"
-                      width={300}
-                      height={200}
-                      style={{
-                        width: "300",
-                        height: "auto",
-                      }}
-                      className="block mx-auto"
-                    />
-                  </div>
-                )}
-                <div className="w-full md:flex-auto px-4 py-3 card">
-                  {post.title && post.title.length > 31 ? (
-                    <h3 className="text-lg font-bold text-gray-700 mb-4">
-                      {post.title.slice(0, 31)}...
-                    </h3>
-                  ) : (
-                    <h3 className="text-lg font-bold text-gray-700 mb-4 border-none ">
-                      {post.title}
-                    </h3>
-                  )}
-                  <p className="text-gray-600 mb-0">
-                    {post.description.length > 72
-                      ? `${post.description.slice(0, 72)}...`
-                      : post.description}
-                  </p>
-                </div>
-              </div>
+        <div className="flex flex-wrap md:flex-nowrap  border w-full border-gray-400 my-8 hover:bg-gray-100">
+          {post.postImage ? (
+            <div className="w-full md:w-auto pt-3 md:pt-0 min-w-[300px]">
+              <Image
+                src={post.postImage.url}
+                alt={post.postImage.altText}
+                width={300}
+                height={200}
+                style={{
+                  width: "300",
+                  height: "auto",
+                }}
+                className="block mx-auto"
+              />
+            </div>
+          ) : (
+            <div className="w-full md:w-auto pt-3 md:pt-0 min-w-[300px]">
+              <Image
+                src="/no_image.jpg"
+                alt="画像の準備中"
+                width={300}
+                height={200}
+                style={{
+                  width: "300",
+                  height: "auto",
+                }}
+                className="block mx-auto"
+              />
+            </div>
+          )}
+          <div className="w-full md:flex-auto px-4 py-3 card">
+            {post.title && post.title.length > 31 ? (
+              <h3 className="text-lg font-bold text-gray-700 mb-4">
+                {post.title.slice(0, 31)}...
+              </h3>
+            ) : (
+              <h3 className="text-lg font-bold text-gray-700 mb-4 border-none ">
+                {post.title}
+              </h3>
+            )}
+            <p className="text-gray-600 mb-0">
+              {post.description.length > 72
+                ? `${post.description.slice(0, 72)}...`
+                : post.description}
+            </p>
+          </div>
+        </div>
       )}
     </>
   );
