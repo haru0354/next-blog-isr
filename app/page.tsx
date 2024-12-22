c78826bcc1261a42e82c0d63228dbbc144bc2fc9
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-12-22 13:14:58 +0900

    トップページの新着記事をディレクトリ移動と修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Section from "./components/1ColumnPage/Section";
 import CategoryTop from "./components/1ColumnPage/CategoryTop";
-import NewArticleTop from "./components/1ColumnPage/NewArticleTop";
+import TopNewArticles from "./components/top-page/TopNewArticles";
 import Footer from "./components/Footer";
 import HeaderImage from "./components/1ColumnPage/HeaderImage";
 
@@ -12,9 +12,7 @@ export default function Home() {
       <HeaderImage />
       <main>
         <div className="w-full">
-          <Section bgColor="bg-sky-50" name="新着記事">
-            <NewArticleTop />
-          </Section>
+          <TopNewArticles />
           <Section bgColor="bg-white" name="カテゴリ">
             <CategoryTop />
           </Section>
