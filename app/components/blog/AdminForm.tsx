443c0713cf8af6987ec718ec1195ed3dca263fb9
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-12-23 12:38:07 +0900

    ログインしていた場合にadminページでリダイレクトするよう設定

--- app/components/blog/AdminForm.tsx
+++ app/components/blog/AdminForm.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import toast from "react-hot-toast";
-
 import Form from "@/app/components/ui/Form";
 import Button from "@/app/components/ui/Button";
 
@@ -15,8 +14,8 @@ const AdminForm = () => {
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const username = formData.get("username")
-    const password = formData.get("password")
+    const username = formData.get("username");
+    const password = formData.get("password");
 
     const result = await signIn("blog", {
       redirect: false,
@@ -25,7 +24,7 @@ const AdminForm = () => {
     });
 
     if (!result?.error) {
-      toast.success("ログインしました！");
+      toast.success("ログインに成功しました！");
       router.replace(`/dashboard/`);
     } else {
       setErrorMessage(result.error);
@@ -33,37 +32,31 @@ const AdminForm = () => {
   };
 
   return (
-    <>
-      <div className="flex items-center justify-center bg-sky-50">
-        <div className="w-[350px] border p-4 my-10 border-gray-400 rounded bg-white">
-          <h2 className="text-center text-base border-b py-1 my-1 bg-white border-gray-400 text-gray-600 font-semibold">
-            ログインフォーム
-          </h2>
-          <div className="flex">
-            <form onSubmit={handleLogin} className="w-full">
-              <Form
-                label="ユーザー名"
-                name="username"
-                placeholder="ユーザー名"
-              />
-              <Form
-                label="パスワード"
-                name="password"
-                placeholder="パスワード"
-                type="password"
-              />
-              {errorMessage && (
-                <p className="text-red-500 pt-4">{errorMessage}</p>
-              )}
-              <Button color="blue" size="auth" className="rounded mt-4">
-                ログイン
-              </Button>
-            </form>
-          </div>
+    <div className="flex items-center justify-center bg-sky-50">
+      <div className="w-[350px] border p-4 my-10 border-gray-400 rounded bg-white">
+        <h2 className="text-center text-base border-b py-1 my-1 bg-white border-gray-400 text-gray-600 font-semibold">
+          ログインフォーム
+        </h2>
+        <div className="flex">
+          <form onSubmit={handleLogin} className="w-full">
+            <Form label="ユーザー名" name="username" placeholder="ユーザー名" />
+            <Form
+              label="パスワード"
+              name="password"
+              placeholder="パスワード"
+              type="password"
+            />
+            {errorMessage && (
+              <p className="text-red-500 pt-4">{errorMessage}</p>
+            )}
+            <Button color="blue" size="auth" className="rounded mt-4">
+              ログイン
+            </Button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default AdminForm
+export default AdminForm;
