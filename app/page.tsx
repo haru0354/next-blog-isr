df70756c1da14fd4ed68c390f7cef184e435f683
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-12-24 16:39:49 +0900

    topPageのヘッダー画像を環境変数から表示非表示を切り替えできるよう変更

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Header from "./components/Header";
 import TopCategories from "./components/top-page/TopCategories";
 import TopNewArticles from "./components/top-page/TopNewArticles";
 import Footer from "./components/Footer";
-import HeaderImage from "./components/1ColumnPage/HeaderImage";
+import HeaderImage from "./components/top-page/HeaderImage";
 
 export default function Home() {
   return (
     <>
       <Header isTopPage={true} />
-      <HeaderImage />
+      {process.env.NEXT_PUBLIC_HEADER_IMAGE === "true" && <HeaderImage />}
       <main>
         <div className="w-full">
           <TopNewArticles />
