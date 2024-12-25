ba184ce2faf23b712afbad6a299c63131314a191
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-12-25 12:16:13 +0900

    HEROセクションを使いまわせるようにしてsectionフォルダへ移動

---
+++ app/components/section/Hero.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+import Button from "../ui/Button";
+
+type HeroProps = {
+  title: string;
+  subTitle: string;
+  contents: string[];
+  href: string;
+};
+
+const Hero: React.FC<HeroProps> = ({ title, subTitle, contents, href }) => {
+  return (
+    <section className="relative bg-sky-200 md:h-[600px] h-[300px] w-full flex items-center">
+      <Image
+        src="/hero_image.JPG"
+        alt="hero背景画像"
+        priority
+        fill
+        style={{
+          objectFit: "cover",
+        }}
+      />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="flex-col justify-center items-center md:w-[440px] w-[340px] md:h-[260px] h-[240px]  py-4 px-2 bg-white md:opacity-90 opacity-80 rounded text-center">
+          <p className="font-semibold mb-3 text-lg md:text-2xl">{title}</p>
+          <p className="font-bold mb-4 text-lg md:text-2xl text-sky-500">
+            「{subTitle}」
+          </p>
+          {contents.map((content) => (
+            <p className="mb-2">{content}</p>
+          ))}
+          <Link href={href}>
+            <Button color="blue" size="normal">
+              詳細はコチラ
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import TopNewArticles from "./components/top-page/TopNewArticles";
 import Footer from "./components/Footer";
 import HeaderImage from "./components/top-page/HeaderImage";
 import RecommendArticle from "./components/section/RecommendArticle";
+import Hero from "./components/1ColumnPage/Hero";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       {process.env.NEXT_PUBLIC_HEADER_IMAGE === "true" && <HeaderImage />}
       <main>
         <div className="w-full">
+         
           <RecommendArticle
             src="/no_image.jpg"
             alt="aa"
@@ -28,6 +30,15 @@ export default function Home() {
             name="aa"
             contents={["bb", "cc", "dd"]}
             imageRight={false}
+          />
+           <Hero
+            title={"aa"}
+            subTitle={"gg"}
+            contents={[
+              "国内旅行・海外旅行で使える",
+              "PC・スマホ・タブレット対応の無料アプリ",
+            ]}
+            href="/"
           />
         </div>
       </main>
