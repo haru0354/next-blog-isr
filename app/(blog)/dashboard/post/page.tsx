b80102c2a974ecfd73e3c37c78c97006817d01a1
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-12-27 12:40:33 +0900

    ListPostsとして使いまわせるようにしてリファクタリング

--- app/(blog)/dashboard/post/page.tsx
+++ app/(blog)/dashboard/post/page.tsx
@@ -1,16 +1,9 @@
 import ListDraftTruePosts from "@/app/components/blog/dashboard/list/ListDraftTruePosts";
-import { getPosts } from "@/app/lib/service/blogServiceMany";
 
-const page = async () => {
-  const posts = await getPosts("category");
-
-  const sortedDraftTruePosts = posts
-    .filter((post) => post.draft === true)
-    .sort((a, b) => b.id - a.id);
-
+const page = () => {
   return (
     <>
-      <ListDraftTruePosts draftTruePosts={sortedDraftTruePosts} />
+      <ListDraftTruePosts />
     </>
   );
 };
--- app/(blog)/dashboard/post/private-post/page.tsx
+++ app/(blog)/dashboard/post/private-post/page.tsx
@@ -1,15 +1,9 @@
-import { getPosts } from "@/app/lib/service/blogServiceMany";
 import ListDraftFalsePosts from "@/app/components/blog/dashboard/list/ListDraftFalsePosts";
 
-const page = async () => {
-  const posts = await getPosts("category");
-
-  const sortedDraftFalsePosts = posts
-    .filter((post) => post.draft === false)
-    .sort((a, b) => b.id - a.id);
+const page = () => {
   return (
     <>
-      <ListDraftFalsePosts draftFalsePosts={sortedDraftFalsePosts} />
+      <ListDraftFalsePosts />
     </>
   );
 };
